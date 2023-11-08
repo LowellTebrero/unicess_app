29974a7133a1b5e8468216c0fe78e3cd8b605e38
LowellTebrero <lowelltebrero@gmail.com>
2023-11-08 12:23:38 +0800

    'update'

--- resources/views/lnu-partials/lnu-mission_vision-section.blade.php
+++ resources/views/lnu-partials/lnu-mission_vision-section.blade.php
@@ -1,4 +1,4 @@
-<section class="flex flex-col sm:flex-row xl:flex-row lg:flex-row md:flex-row mission-vision h-[50vh]">
+<section class="flex flex-col sm:flex-row md:flex-row mission-vision h-[50vh]">
 
     <div class="m-s flex flex-col p-10 sm:space-y-10 md:space-y-10 lg:space-y-10  xl:space-y-4 2xl:space-y-8 xl:mt-0 2xl:mt-12">
         <div class="sm:mt-5 xl:mt-0">
@@ -17,8 +17,8 @@
 
 
     <div class="img flex flex-col items-center justify-center">
-        <img class=" w-[3rem] sm:w-[7rem] lg:w-[10rem] xl:w-[12rem]" src="{{ asset('img/logo.png') }}"  alt="">
-        <h1 class=" sm:mt-5  text-white sm:text-lg font-bold text-xs md:text-2xl lg:text-2xl  xl:text-2xl">UNICESS 2023</h1>
+        <img class="w-[3rem] sm:w-[7rem] lg:w-[10rem] xl:w-[12rem]" src="{{ asset('img/logo.png') }}">
+        <h1 class="sm:mt-5  text-white sm:text-lg font-bold text-xs md:text-2xl lg:text-2xl  xl:text-2xl">UNICESS 2023</h1>
     </div>
 
 </section>
