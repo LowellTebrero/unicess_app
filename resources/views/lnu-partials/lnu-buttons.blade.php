505db456219a5bc08ab740b258231d20dae7d902
LowellTebrero <lowelltebrero@gmail.com>
2023-11-08 11:42:37 +0800

    'update'

--- resources/views/lnu-partials/lnu-buttons.blade.php
+++ resources/views/lnu-partials/lnu-buttons.blade.php
@@ -1,22 +1,22 @@
 
 {{--  University Updates  --}}
 <section class="bg-slate-100">
-  <div class="min-h-[55vh] flex-col space-y-8  sm:flex-row  sm:space-x-4 space-x-0  flex items-center mx-4 justify-center p-8 sm:p-0 md:mx-16   sm:space-y-0  md:space-y-0 lg:space-y-0 xl:space-y-0 sm:mx-8  md:space-x-6   lg:space-x-16 ">
+  <div class="min-h-[55vh] flex items-center justify-center flex-col mx-4 space-x-0  space-y-8 sm:flex-row  sm:space-x-4  p-8 sm:p-0 md:mx-16 sm:space-y-0  md:space-y-0 lg:space-y-0 sm:mx-8  md:space-x-6 lg:space-x-4 ">
 
     {{--  <img class="absolute h-full w-full opacity-0 md:opacity-0 lg:opacity-100" src="{{ asset('img/arrow-bg.png') }}" alt="">  --}}
 
         <a  href="#" class="text-lg w-full  xl:text-lg  sm:text-[1.1rem] 2xl:text-2xl bg-white xl:w-64 drop-shadow-xl justify-center p-2  rounded-lg flex flex-col text-blue-900 items-center  min-h-[23vh] font-bold hover:bg-gray-100">
-            <img class="my-3 sm:w-[3.4rem] w-[2rem] lg:w-[5rem]" src="{{ asset('img/partners.png') }}" alt="">
+            <img class="my-3 sm:w-[3.4rem] w-[2rem] lg:w-[3rem]" src="{{ asset('img/partners.png') }}" alt="">
             Partners
         </a>
 
         <a href="#" class=" xl:text-lg h-[5vh] text-lg sm:text-[1.1rem] 2xl:text-2xl bg-white w-full xl:w-64  drop-shadow-xl justify-center p-2   rounded-lg flex flex-col text-blue-900 items-center  min-h-[23vh] font-bold hover:bg-gray-100">
-            <img class="my-3 sm:w-[3.4rem] w-[2rem] lg:w-[5rem]" src="{{ asset('img/beneficiary.png') }}" alt="">
+            <img class="my-3 sm:w-[3.4rem] w-[2rem] lg:w-[3rem]" src="{{ asset('img/beneficiary.png') }}" alt="">
             Beneficiary
         </a>
 
         <a class="xl:text-lg 2xl:text-2xl text-lg sm:text-[1.1rem] bg-white w-full xl:w-64  drop-shadow-xl justify-center p-2 rounded-lg flex flex-col text-blue-900 items-center  min-h-[23vh] font-bold hover:bg-gray-100">
-            <img class="my-3 sm:w-[3.4rem] w-[2rem] lg:w-[5rem]" src="{{ asset('img/faculties.png') }}" alt="">
+            <img class="my-3 sm:w-[3.4rem] w-[2rem] lg:w-[3rem]" src="{{ asset('img/faculties.png') }}" alt="">
             Faculties
         </a>
     </div>
