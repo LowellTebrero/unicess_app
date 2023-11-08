b09e0ff1499eb7d52a9e4054258aac8649ef6d95
LowellTebrero <lowelltebrero@gmail.com>
2023-11-08 12:11:10 +0800

    'update'

--- resources/views/lnu-partials/lnu-buttons.blade.php
+++ resources/views/lnu-partials/lnu-buttons.blade.php
@@ -1,21 +1,21 @@
 
 {{--  University Updates  --}}
 <section class="bg-slate-100">
-  <div class="min-h-[55vh] flex items-center justify-center sm:p-8 sm:mx-8 mx-4 p-4 py-10  flex-col space-x-4 space-y-4 sm:space-y-0 sm:flex-row sm:space-x-2">
+  <div class="min-h-[55vh] flex items-center justify-center sm:p-8 sm:mx-8 mx-4 p-8 py-10  flex-col sm:flex-row ">
 
     {{--  <img class="absolute h-full w-full opacity-0 md:opacity-0 lg:opacity-100" src="{{ asset('img/arrow-bg.png') }}" alt="">  --}}
 
-        <a  href="#" class="text-lg w-full  xl:text-lg  sm:text-[1.1rem] 2xl:text-2xl bg-white xl:w-64 drop-shadow-xl justify-center p-2  rounded-lg flex flex-col text-blue-900 items-center  min-h-[23vh] font-bold hover:bg-gray-100">
+        <a  href="#" class="m-4 text-lg w-full  xl:text-lg  sm:text-[1.1rem] 2xl:text-2xl bg-white xl:w-64 drop-shadow-xl justify-center p-2  rounded-lg flex flex-col text-blue-900 items-center  min-h-[23vh] font-bold hover:bg-gray-100">
             <img class="my-3 sm:w-[3.4rem] w-[2rem] lg:w-[3rem]" src="{{ asset('img/partners.png') }}" alt="">
             Partners
         </a>
 
-        <a href="#" class=" xl:text-lg h-[5vh] text-lg sm:text-[1.1rem] 2xl:text-2xl bg-white w-full xl:w-64  drop-shadow-xl justify-center p-2   rounded-lg flex flex-col text-blue-900 items-center  min-h-[23vh] font-bold hover:bg-gray-100">
+        <a href="#" class="m-4 xl:text-lg h-[5vh] text-lg sm:text-[1.1rem] 2xl:text-2xl bg-white w-full xl:w-64  drop-shadow-xl justify-center p-2   rounded-lg flex flex-col text-blue-900 items-center  min-h-[23vh] font-bold hover:bg-gray-100">
             <img class="my-3 sm:w-[3.4rem] w-[2rem] lg:w-[3rem]" src="{{ asset('img/beneficiary.png') }}" alt="">
             Beneficiary
         </a>
 
-        <a class="xl:text-lg 2xl:text-2xl text-lg sm:text-[1.1rem] bg-white w-full xl:w-64  drop-shadow-xl justify-center p-2 rounded-lg flex flex-col text-blue-900 items-center  min-h-[23vh] font-bold hover:bg-gray-100">
+        <a class="m-4 xl:text-lg 2xl:text-2xl text-lg sm:text-[1.1rem] bg-white w-full xl:w-64  drop-shadow-xl justify-center p-2 rounded-lg flex flex-col text-blue-900 items-center  min-h-[23vh] font-bold hover:bg-gray-100">
             <img class="my-3 sm:w-[3.4rem] w-[2rem] lg:w-[3rem]" src="{{ asset('img/faculties.png') }}" alt="">
             Faculties
         </a>
