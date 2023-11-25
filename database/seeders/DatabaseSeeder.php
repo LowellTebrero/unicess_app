c7948315db8d88fbc4f9d425b729c11b1d503ef5
Yvan Sabay <sabayyvan2018@gmail.com>
2023-11-25 20:06:17 +0800

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,20 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
-        $this->call(CustomizeSeeder::class);
-        $this->call(RoleandPermissionSeeder::class);
-        $this->call(ProgramSeeder::class);
-        $this->call(FacultySeeder::class);
-        $this->call(CesoRoleSeeder::class);
-        $this->call(PartnerSeeder::class);
-        $this->call(LocationSeeder::class);
-        $this->call(ParticipationNameSeeder::class);
-        $this->call(YearSeeder::class);
-
-         $this->call(AdminSeeder::class);
-
-
-
+        $this->call([
+            CustomizeSeeder::class,
+            RoleandPermissionSeeder::class,
+            ProgramSeeder::class,
+            FacultySeeder::class,
+            CesoRoleSeeder::class,
+            PartnerSeeder::class,
+            LocationSeeder::class,
+            ParticipationNameSeeder::class,
+            YearSeeder::class,
+        ]);
     }
 }
