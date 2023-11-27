eae700f1f2441a5d9d94626f68986d556f91984c
LowellTebrero <lowelltebrero@gmail.com>
2023-11-27 21:24:05 +0800

    'update'

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -180,7 +180,9 @@
                                 {{--  <x-dropdown-link :href="route('admin.dashboard.index')" class="dynamic-link">
                                     {{ __('Dashboard') }}
                                 </x-dropdown-link>  --}}
-                                <a href={{ route('admin.dashboard.index') }}>AdminDashboard</a>
+                                <a href={{ route('admin.dashboard.index') }}
+                                class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+                                Dashboard</a>
 
                                 @else
                                 <x-dropdown-link :href="route('User-dashboard.index')" class="dynamic-link">
