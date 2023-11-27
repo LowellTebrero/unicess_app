6ea3be0de2f56d8f94b7f9bf2188889589efe9ba
LowellTebrero <lowelltebrero@gmail.com>
2023-11-27 19:48:06 +0800

    'update'

--- resources/views/lnu.blade.php
+++ resources/views/lnu.blade.php
@@ -14,7 +14,6 @@
 
         <!-- Scripts -->
         <script src="//unpkg.com/alpinejs" defer></script>
-        <script src="{{ asset('js/chatPlugin.js') }}"></script>
         @vite(['resources/css/app.css'])
         {{--  'resources/css/preloader.css'  --}}
 
@@ -96,7 +95,7 @@
 
 
         <x-messages/>
-
+        <script src="{{ asset('js/chatPlugin.js') }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="{{ asset('js/preloader.js') }}"></script>
