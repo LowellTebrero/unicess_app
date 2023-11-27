1d63927f20fc655ca9b27fc1696a1fd7b76cbbc9
Yvan Sabay <sabayyvan2018@gmail.com>
2023-11-27 21:09:57 +0800

    hello

--- resources/views/auth/welcome-user.blade.php
+++ resources/views/auth/welcome-user.blade.php
@@ -5,7 +5,7 @@
 
 <x-welcome-user-layout>
 <section class="flex flex-col items-center space-y-4">
-   <h1 class="text-5xl text-yellow-400 font-medium"> Hi! Welcome to UniCESS</h1>
+   <h1 class="text-5xl text-yellow-400 font-medium">Hello! Welcome to UniCESS</h1>
    <h4 class="tracking-wider font-medium pb-5">Let's setup your Profile Information before we proceed to homepage</h4>
    <a class="bg-blue-500 px-10 py-5 rounded-lg hover:bg-blue-600 text-white font-medium" href={{ route('profile.partials.edit-auth-profile', $user->id) }}>
     Get started
