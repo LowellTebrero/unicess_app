2da73cc85f64b1eca14fe58792a9c30a47af9272
LowellTebrero <lowelltebrero@gmail.com>
2023-11-27 19:20:17 +0800

    'update'

--- resources/views/lnu.blade.php
+++ resources/views/lnu.blade.php
@@ -105,13 +105,12 @@
     <x-messages/>
 
 
-
+    <script src="{{ asset('js/preloader.js') }}"></script>
+    <script src="{{ asset('js/chatPlugin.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
     integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></>
-    <script src="{{ asset('js/preloader.js') }}"></script>
-    <script src="{{ asset('js/chatPlugin.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
 
 
