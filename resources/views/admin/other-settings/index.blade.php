afa70a4a529513c1ce0cf5cd21684d7b01758c3f
LowellTebrero <lowelltebrero@gmail.com>
2024-01-05 23:55:26 +0800

    'update'

--- resources/views/admin/other-settings/index.blade.php
+++ resources/views/admin/other-settings/index.blade.php
@@ -26,7 +26,7 @@
                     <div class="p-5 flex justify-between">
                         <div>
                             <h1 class="tracking-wider font-medium text-gray-600 text-xs 2xl:text-base">UPDATE PROPOSAL TEMPLATE HERE</h1>
-                            <h1 class="tracking-wider text-[.7rem] 2xl:text-xs">Note: (.docx/doc) only</h1>
+
                         </div>
 
                             @if ($templates->isEmpty())
@@ -37,7 +37,7 @@
                             @else
 
                             <button data-modal-target="default-modal" data-modal-toggle="default-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                            Add File
+                            Add File(s)
                             </button>
                             @endif
 
