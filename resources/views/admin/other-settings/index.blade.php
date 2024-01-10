ab041052d148a91af0c952024919a1e06ff0f023
LowellTebrero <lowelltebrero@gmail.com>
2024-01-10 13:36:11 +0800

    'update'

--- resources/views/admin/other-settings/index.blade.php
+++ resources/views/admin/other-settings/index.blade.php
@@ -36,7 +36,7 @@
                             </button>
                             @else
 
-                            <button data-modal-target="default-modal" data-modal-toggle="default-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                            <button data-modal-target="default-modal" data-modal-toggle="default-modal" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs 2xl:text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
                             Add File(s)
                             </button>
                             @endif
