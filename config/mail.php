6fe050c1877f2d5f73ebb9cf7da36ed191f7cab1
LowellTebrero <lowelltebrero@gmail.com>
2024-01-23 13:13:03 +0800

    'update'

--- config/mail.php
+++ config/mail.php
@@ -36,13 +36,14 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtpout.secureserver.net'),
+            'host' => env('MAIL_HOST', 'smtp.googlemail.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'auth' => 'login',
         ],
 
         'ses' => [
