af4f380a27e2b9f7f534fed09414edbb1a275ed1
LowellTebrero <lowelltebrero@gmail.com>
2024-02-12 20:56:06 +0800

    'update'

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -11,8 +11,7 @@ use Illuminate\Validation\Rule;
 
 class ImageController extends Controller
 {
-    public function upload(Request $request, $id)
-    {
+    public function upload(Request $request, $id ){
         // Validate the uploaded file
         $request->validate([
             'avatar' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Adjust validation rules as needed
@@ -20,31 +19,29 @@ class ImageController extends Controller
 
         $user = User::find($id);
 
-        // Update the user's avatar URL in the database
-        if ($request->hasFile('avatar')) {
-            $image = $request->file('avatar');
-            $filename = $image->hashName(); // Remove '.png' from here since it's already included in the hash name
-            $resize_image = Image::make($image->getRealPath())->resize(500, 500);
 
-            // Save the resized image
+    // Update the user's avatar URL in the database
+        if($request->file('avatar')){
+            $image = $request->file('avatar');
+            $filename = Str::limit($image->hashName(), 10).'.'.$image->getClientOriginalExtension();
+            $resize_image = Image::make($image->getRealPath());
+            $resize_image->resize(500, 500);
             $resize_image->save(public_path('upload/image-folder/profile-image/'. $filename));
 
-            // Delete the old avatar file if it exists
-            if (File::exists(public_path('upload/image-folder/profile-image/'. $user->avatar))) {
-                File::delete(public_path('upload/image-folder/profile-image/'. $user->avatar));
-            }
-
-            $user->avatar = $filename;
-            $user->save();
-
-            return response()->json([
-                'message' => 'Avatar updated successfully',
-                'avatar_url' => asset('upload/image-folder/profile-image/'. $filename), // Use 'asset' to generate correct URL
-            ]);
-        } else {
-            return response()->json([
-                'message' => 'No avatar provided',
-            ], 400);
+        if(File::exists($resize_image)){
+            unlink($resize_image);
         }
+
+        $user->avatar = $filename;
+        $user->save();
+
+
+        return response()->json([
+            'message' => 'Avatar updated successfully',
+            'avatar_url' => (public_path('upload/image-folder/profile-image/'. $filename)),
+        ]);
+
+        }
+
     }
 }
