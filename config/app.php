a7b069c5ddbce17ad7414044d2fa098cfaa90336
LowellTebrero <lowelltebrero@gmail.com>
2024-02-18 22:39:19 +0800

    'update'

--- config/app.php
+++ config/app.php
@@ -182,7 +182,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
-        // Stevebauman\Location\LocationServiceProvider::class,
+        Stevebauman\Location\LocationServiceProvider::class,
 
         /*
          * Package Service Providers...
